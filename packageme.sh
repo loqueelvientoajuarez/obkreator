4e8572f747f1466d91278ea65eb64359d1060efd
loqueelvientoajuarez <regis.lachaume@gmail.com>
2022-11-23 18:10:14 -0300

    + details

--- packageme.sh
+++ packageme.sh
@@ -4,7 +4,9 @@ rm dist/*
 rm build/*
 rm *egg-info/*
 
-python3 setup.py sdist bdist_wheel
+python3 -m pip install --upgrade build
+python3 -m build
+
+python3 -m pip install --upgrade twine
 python3 -m twine upload -u loqueelvientoajuarez -p "tplpqlp[" dist/*
 # python3 -m pip install --upgrade obkreator
-
